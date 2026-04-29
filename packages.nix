1ffa610bb2a6116f675d25bd2a34ea4961cf01c0
ilaumjd <ilaumjd@gmail.com>
2026-04-29 23:52:28 +0700

    nix: add packages.nix

---
+++ packages.nix
@@ -0,0 +1,34 @@
+let
+  nixpkgs = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/nixos-25.11.tar.gz";
+  };
+  pkgs = import nixpkgs {};
+in
+pkgs.buildEnv {
+  name = "user-packages";
+  paths = with pkgs; [
+    atuin
+    bat
+    btop
+    carapace
+    curl
+    fastfetch
+    fd
+    fzf
+    git
+    htop
+    jq
+    lazydocker
+    lazygit
+    neovim
+    patch
+    ripgrep
+    screen
+    sqlite
+    stow
+    tmux
+    vim
+    zoxide
+  ];
+}
+
